2b5003061bf5c4d692afe867b8a7b8feb3939b93
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-08 12:29:26 -0700

    updating the surf tag checking

--- ruckus.tcl
+++ ruckus.tcl
@@ -3,7 +3,7 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 ## Check for submodule tagging
 if { [SubmoduleCheck {ruckus} {1.3.5} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}   {1.3.6} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}   {1.3.7} ] < 0 } {exit -1}
 
 ## Check for version 2016.4 of Vivado
 if { [VersionCheck 2016.4] < 0 } {exit -1}

e10a713212b307b2598d8014744fe8a24a806d4b
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-08 11:42:32 -0700

    updating the ruckus tag checking

--- ruckus.tcl
+++ ruckus.tcl
@@ -2,7 +2,7 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 ## Check for submodule tagging
-if { [SubmoduleCheck {ruckus} {1.3.4} ] < 0 } {exit -1}
+if { [SubmoduleCheck {ruckus} {1.3.5} ] < 0 } {exit -1}
 if { [SubmoduleCheck {surf}   {1.3.6} ] < 0 } {exit -1}
 
 ## Check for version 2016.4 of Vivado

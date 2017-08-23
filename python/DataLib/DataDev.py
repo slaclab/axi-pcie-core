07287f35923e82835ed27707193d3c90d437c36e
Larry Ruckman <ruckman@slac.stanford.edu>
2017-08-23 15:09:05 -0700

    adding SPI and BPI support

--- python/DataLib/DataDev.py
+++ python/DataLib/DataDev.py
@@ -19,16 +19,36 @@
 
 import pyrogue as pr
 from surf.axi import *
+from surf.devices.micron import *
 
 class DataDev(pr.Device):
     def __init__(   self,       
             name        = "DataDev",
             description = "Container for data development registers",
+            useBpi      = False,
+            useSpi      = False,
             **kwargs):
         super().__init__(name=name, description=description, **kwargs)
 
-        # Axi Version
+        if (useBpi):
+            self.add(_AxiMicronP30(
+                offset       =  0x0000,
+                expand       =  False,                                    
+                hidden       =  True,                                    
+            ))         
+        
+        if (useSpi):
+            for i in range(2):
+                self.add(AxiMicronN25Q(
+                    name         = "AxiMicronN25Q[%i]" % (i),
+                    offset       =  0x1000 + (i * 0x1000),
+                    description  = "AxiMicronN25Q: %i" % (i),                                
+                    expand       =  False,                                    
+                    hidden       =  True,                                    
+                ))        
+        
         self.add(AxiVersion(            
             offset       = 0x30000, 
             expand       = False,
-        ))        
+        ))
+        

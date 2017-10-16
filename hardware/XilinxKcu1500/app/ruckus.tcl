c0c8231f03c0c625282e8ae8d1f4cb33cb03f927
Larry Ruckman <ruckman@slac.stanford.edu>
2017-10-16 15:05:50 -0700

    reorg

--- hardware/XilinxKcu1500/app/ruckus.tcl
+++ hardware/XilinxKcu1500/app/ruckus.tcl
@@ -2,9 +2,4 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load local Source Code and Constraints
-loadSource      -dir  "$::DIR_PATH/rtl/"
-
-# Check for  partial reconfiguration
-if { [info exists ::env(BYPASS_RECONFIG)] != 1 || $::env(BYPASS_RECONFIG) == 0 } {
-   loadConstraints -path "$::DIR_PATH/xdc/XilinxKcu1500Reconfig.xdc"
-}
+loadSource -dir "$::DIR_PATH/pgp2b"

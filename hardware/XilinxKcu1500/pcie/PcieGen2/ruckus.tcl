c20f5a79b81baaef64daddde458cab1c4fe1e5b6
Larry Ruckman <ruckman@slac.stanford.edu>
2017-10-18 20:14:37 -0700

    converting from PCIe XCI files to DCP files

--- hardware/XilinxKcu1500/pcie/PcieGen2/ruckus.tcl
+++ hardware/XilinxKcu1500/pcie/PcieGen2/ruckus.tcl
@@ -6,16 +6,17 @@ loadSource -dir "$::DIR_PATH/rtl"
 
 # Check for partial reconfiguration
 if { [info exists ::env(BYPASS_RECONFIG)] != 1 || $::env(BYPASS_RECONFIG) == 0 } {
-   loadSource -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhyWrapper.vhd"
-   loadIpCore -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhy.xci"
+   loadSource      -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhyWrapper.vhd"
+   loadConstraints -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhy.xdc"
+   # loadIpCore    -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhy.xci"
+   loadSource      -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhy.dcp"
 } else {
-   loadSource -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhyWrapper.vhd"
-   loadIpCore -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhy.xci"
+   loadSource      -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhyWrapper.vhd"
+   loadConstraints -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhy.xdc"
+   # loadIpCore    -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhy.xci"
+   loadSource      -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhy.dcp"
 }
 
-# Check for TIG
-if { [info exists ::env(TIG)] != 1 || $::env(TIG) == 0 } {
-   loadConstraints -path "$::DIR_PATH/WithPartialReconfig/XilinxKcu1500PciePhy.xdc"
-} else {
-   loadConstraints -path "$::DIR_PATH/WithoutPartialReconfig/XilinxKcu1500PciePhy.xdc"
-}
+set_property PROCESSING_ORDER {EARLY}                         [get_files {XilinxKcu1500PciePhy.xdc}]
+set_property SCOPED_TO_REF    {XilinxKcu1500PciePhy_pcie3_ip} [get_files {XilinxKcu1500PciePhy.xdc}]
+set_property SCOPED_TO_CELLS  {inst}                          [get_files {XilinxKcu1500PciePhy.xdc}]
--- hardware/XilinxKcu1500/pcie/PcieGen3/rtl/AxiPciePkg.vhd
+++ hardware/XilinxKcu1500/pcie/PcieGen3/rtl/AxiPciePkg.vhd
@@ -19,12 +19,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
 use work.AxiStreamPkg.all;
 use work.AxiPkg.all;
 
 package AxiPciePkg is
 
    constant TPD_C : time := 1 ns;
+   
+   -- Application BAR0 address space   
+   constant BAR0_BASE_ADDR_G  : slv(31 downto 0) := x"0080_0000";
+   constant BAR0_HIGH_ADDR_G  : slv(31 downto 0) := x"00FF_FFFF";
+   constant BAR0_ERROR_RESP_C : slv(1 downto 0)  := AXI_RESP_OK_C;  -- Always return OK to a MMAP() 
 
    -- System Clock Frequency
    constant SYS_CLK_FREQ_C : real := 250.0E+6;  -- units of Hz

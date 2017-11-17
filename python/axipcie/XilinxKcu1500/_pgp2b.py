05e7753da1c8452d38c5fb11ae3214e109bcc736
Benjamin Reese <bareese@slac.stanford.edu>
2017-11-16 16:56:48 -0800

    Set GTH reconfig values by disp so that enums can be auto applied

--- python/axipcie/XilinxKcu1500/_pgp2b.py
+++ python/axipcie/XilinxKcu1500/_pgp2b.py
@@ -3,54 +3,54 @@ import surf
 
 LINK_SPEED_TABLE = {
     '3.125' : {
-        'CPLL_CFG0'      : 0x21F8,
-        'CPLL_CFG2'      : 0x0004,
-        'CPLL_FBDIV'     : 5,
-        'PMA_RSV1'       : 0xE000,
-        'RXCDR_CFG2_GEN3'     : 0x0756,
-        'RXOUT_DIV'      : 2,
-        'TXOUT_DIV'      : 2,
-        'TX_PROGDIV_CFG' : 57762, #20.0        
+        'CPLL_CFG0'      : '0x21F8',
+        'CPLL_CFG2'      : '0x0004',
+        'CPLL_FBDIV'     : '5',
+        'PMA_RSV1'       : '0xE000',
+        'RXCDR_CFG2_GEN3': '0x0756',
+        'RXOUT_DIV'      : '2',
+        'TXOUT_DIV'      : '2',
+        'TX_PROGDIV_CFG' : '20.0',
     },
     '2.5' : {
-        'CPLL_CFG0'      : 0x67F8,
-        'CPLL_CFG2'      : 0x0007,
-        'CPLL_FBDIV'     : 4,
-        'PMA_RSV1'       : 0xF000,
-        'RXCDR_CFG2_GEN3'	 : 0x0756,
-        'RXOUT_DIV'      : 2,
-        'TXOUT_DIV'      : 2,
-        'TX_PROGDIV_CFG' : 57762, #20.0
+        'CPLL_CFG0'      : '0x67F8',
+        'CPLL_CFG2'      : '0x0007',
+        'CPLL_FBDIV'     : '4',
+        'PMA_RSV1'       : '0xF000',
+        'RXCDR_CFG2_GEN3': '0x0756',
+        'RXOUT_DIV'      : '2',
+        'TXOUT_DIV'      : '2',
+        'TX_PROGDIV_CFG' : '20.0',
     },
     '1.25' : {
-        'CPLL_CFG0'      : 0x67F8 ,
-        'CPLL_CFG2'      : 0x0007 ,
-        'CPLL_FBDIV'     : 4,
-        'PMA_RSV1'       : 0xF000,
-        'RXCDR_CFG2_GEN3'	 : 0x0746,
-        'RXOUT_DIV'      : 4,
-        'TXOUT_DIV'      : 4,
-        'TX_PROGDIV_CFG' : 57766, #40.0        
+        'CPLL_CFG0'      : '0x67F8' ,
+        'CPLL_CFG2'      : '0x0007' ,
+        'CPLL_FBDIV'     : '4',
+        'PMA_RSV1'       : '0xF000',
+        'RXCDR_CFG2_GEN3': '0x0746',
+        'RXOUT_DIV'      : '4',
+        'TXOUT_DIV'      : '4',
+        'TX_PROGDIV_CFG' : '40.0',
     },
     '5.0' : {
-        'CPLL_CFG0'      : 0x67F8 ,
-        'CPLL_CFG2'      : 0x0007 ,
-        'CPLL_FBDIV'     : 4,
-        'PMA_RSV1'       : 0xF000,
-        'RXCDR_CFG2_GEN3'	 : 0x0766,
-        'RXOUT_DIV'      : 1,
-        'TXOUT_DIV'      : 1,
-        'TX_PROGDIV_CFG' : 57760, #10.0
+        'CPLL_CFG0'      : '0x67F8' ,
+        'CPLL_CFG2'      : '0x0007' ,
+        'CPLL_FBDIV'     : '4',
+        'PMA_RSV1'       : '0xF000',
+        'RXCDR_CFG2_GEN3': '0x0766',
+        'RXOUT_DIV'      : '1',
+        'TXOUT_DIV'      : '1',
+        'TX_PROGDIV_CFG' : '10.0',
     },
     '6.125' : {
-        'CPLL_CFG0'      : 0x21F8,
-        'CPLL_CFG2'      : 0x0004,
-        'CPLL_FBDIV'     : 5,
-        'PMA_RSV1'       : 0xe000,
-        'RXCDR_CFG2_GEN3'	 : 0x0766,
-        'RXOUT_DIV'      : 1,
-        'TXOUT_DIV'      : 1,
-        'TX_PROGDIV_CFG' : 57760, #10.0
+        'CPLL_CFG0'      : '0x21F8',
+        'CPLL_CFG2'      : '0x0004',
+        'CPLL_FBDIV'     : '5',
+        'PMA_RSV1'       : '0xe000',
+        'RXCDR_CFG2_GEN3': '0x0766',
+        'RXOUT_DIV'      : '1',
+        'TXOUT_DIV'      : '1',
+        'TX_PROGDIV_CFG' : '10.0',
     }
 }
 
@@ -81,9 +81,13 @@ class AppPgp2bLane(pr.Device):
         self._linkRate = value
         params = LINK_SPEED_TABLE[value]
         for k,v in params.items():
-            self.Gthe3Channel.node(k).set(v)
+            if isinstance(v, str):
+                self.Gthe3Channel.node(k).setDisp(v)
+            else:
+                self.Gthe3Channel.node(k).set(v)
         self.Pgp2bAxi.ResetRx()
-        self.Pgp2bAxi.ResetTx()        
+        self.Pgp2bAxi.ResetTx()
+        self.Pgp2bAxi.ResetGt()
         # Need to reset when done
 
 class AppPgp2bQuad(pr.Device):

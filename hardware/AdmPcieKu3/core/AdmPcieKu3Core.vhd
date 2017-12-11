3e5593e211d81981d020e5140b713ab70a2cc086
Larry Ruckman <ruckman@slac.stanford.edu>
2017-12-11 09:49:48 -0800

    removing obsolete PROM interface

--- hardware/AdmPcieKu3/core/AdmPcieKu3Core.vhd
+++ hardware/AdmPcieKu3/core/AdmPcieKu3Core.vhd
@@ -2,7 +2,7 @@
 -- File       : AdmPcieKu3Core.vhd
 -- Company    : SLAC National Accelerator Laboratory
 -- Created    : 2017-04-06
--- Last update: 2017-12-06
+-- Last update: 2017-12-11
 -------------------------------------------------------------------------------
 -- Description: AXI PCIe Core for ADM-PCIE-KU3 board 
 --
@@ -56,44 +56,37 @@ entity AdmPcieKu3Core is
       --  Top Level Interfaces
       ------------------------
       -- System Interface
-      sysClk         : out   sl;
-      sysRst         : out   sl;
+      sysClk         : out sl;
+      sysRst         : out sl;
       -- DMA Interfaces  (sysClk domain)
-      dmaObMasters   : out   AxiStreamMasterArray(DMA_SIZE_G-1 downto 0);
-      dmaObSlaves    : in    AxiStreamSlaveArray(DMA_SIZE_G-1 downto 0);
-      dmaIbMasters   : in    AxiStreamMasterArray(DMA_SIZE_G-1 downto 0);
-      dmaIbSlaves    : out   AxiStreamSlaveArray(DMA_SIZE_G-1 downto 0);
+      dmaObMasters   : out AxiStreamMasterArray(DMA_SIZE_G-1 downto 0);
+      dmaObSlaves    : in  AxiStreamSlaveArray(DMA_SIZE_G-1 downto 0);
+      dmaIbMasters   : in  AxiStreamMasterArray(DMA_SIZE_G-1 downto 0);
+      dmaIbSlaves    : out AxiStreamSlaveArray(DMA_SIZE_G-1 downto 0);
       -- Application AXI-Lite Interfaces [0x00800000:0x00FFFFFF] (appClk domain)
-      appClk         : in    sl;
-      appRst         : in    sl;
-      appReadMaster  : out   AxiLiteReadMasterType;
-      appReadSlave   : in    AxiLiteReadSlaveType;
-      appWriteMaster : out   AxiLiteWriteMasterType;
-      appWriteSlave  : in    AxiLiteWriteSlaveType;
+      appClk         : in  sl;
+      appRst         : in  sl;
+      appReadMaster  : out AxiLiteReadMasterType;
+      appReadSlave   : in  AxiLiteReadSlaveType;
+      appWriteMaster : out AxiLiteWriteMasterType;
+      appWriteSlave  : in  AxiLiteWriteSlaveType;
       -------------------
       --  Top Level Ports
       -------------------      
       -- QSFP[0] Ports
-      qsfp0RstL      : out   sl;
-      qsfp0LpMode    : out   sl;
+      qsfp0RstL      : out sl;
+      qsfp0LpMode    : out sl;
       -- QSFP[1] Ports
-      qsfp1RstL      : out   sl;
-      qsfp1LpMode    : out   sl;
-      -- Boot Memory Ports 
-      flashAddr      : out   slv(23 downto 0);
-      flashData      : inout slv(15 downto 4);
-      flashAdv       : out   sl;
-      flashOeL       : out   sl;
-      flashWeL       : out   sl;
-      flashRstL      : out   sl;
+      qsfp1RstL      : out sl;
+      qsfp1LpMode    : out sl;
       -- PCIe Ports 
-      pciRstL        : in    sl;
-      pciRefClkP     : in    sl;
-      pciRefClkN     : in    sl;
-      pciRxP         : in    slv(7 downto 0);
-      pciRxN         : in    slv(7 downto 0);
-      pciTxP         : out   slv(7 downto 0);
-      pciTxN         : out   slv(7 downto 0));
+      pciRstL        : in  sl;
+      pciRefClkP     : in  sl;
+      pciRefClkN     : in  sl;
+      pciRxP         : in  slv(7 downto 0);
+      pciRxN         : in  slv(7 downto 0);
+      pciTxP         : out slv(7 downto 0);
+      pciTxN         : out slv(7 downto 0));
 end AdmPcieKu3Core;
 
 architecture mapping of AdmPcieKu3Core is
@@ -120,18 +113,11 @@ architecture mapping of AdmPcieKu3Core is
    signal phyWriteMaster : AxiLiteWriteMasterType;
    signal phyWriteSlave  : AxiLiteWriteSlaveType;
 
-   signal flashAddress : slv(28 downto 0);
-   signal flashDin     : slv(15 downto 0);
-   signal flashDout    : slv(15 downto 0);
-   signal flashTri     : sl;
-
    signal sysClock    : sl;
    signal sysReset    : sl;
    signal systemReset : sl;
    signal cardReset   : sl;
    signal dmaIrq      : sl;
-   signal flashCeL    : sl;
-   signal flashClk    : sl;
 
 begin
 
@@ -224,53 +210,7 @@ begin
          appWriteSlave      => appWriteSlave,
          -- Application Force reset
          cardResetOut       => cardReset,
-         cardResetIn        => systemReset,
-         -- Boot Memory Ports 
-         bpiAddr            => flashAddress,
-         bpiAdv             => flashAdv,
-         bpiClk             => flashClk,
-         bpiRstL            => flashRstL,
-         bpiCeL             => flashCeL,
-         bpiOeL             => flashOeL,
-         bpiWeL             => flashWeL,
-         bpiDin             => flashDin,
-         bpiDout            => flashDout,
-         bpiTri             => flashTri);
-
-   flashAddr <= flashAddress(23 downto 0);
-
-   U_STARTUPE3 : STARTUPE3
-      generic map (
-         PROG_USR      => "FALSE",  -- Activate program event security feature. Requires encrypted bitstreams.
-         SIM_CCLK_FREQ => 0.0)  -- Set the Configuration Clock Frequency(ns) for simulation
-      port map (
-         CFGCLK    => open,  -- 1-bit output: Configuration main clock output
-         CFGMCLK   => open,  -- 1-bit output: Configuration internal oscillator clock output
-         DI        => flashDout(3 downto 0),  -- 4-bit output: Allow receiving on the D[3:0] input pins
-         EOS       => open,  -- 1-bit output: Active high output signal indicating the End Of Startup.
-         PREQ      => open,  -- 1-bit output: PROGRAM request to fabric output         
-         DO        => flashDin(3 downto 0),  -- 4-bit input: Allows control of the D[3:0] pin outputs
-         DTS       => (others => flashTri),  -- 4-bit input: Allows tristate of the D[3:0] pins
-         FCSBO     => flashCeL,  -- 1-bit input: Contols the FCS_B pin for flash access
-         FCSBTS    => '0',              -- 1-bit input: Tristate the FCS_B pin
-         GSR       => '0',  -- 1-bit input: Global Set/Reset input (GSR cannot be used for the port name)
-         GTS       => '0',  -- 1-bit input: Global 3-state input (GTS cannot be used for the port name)
-         KEYCLEARB => '0',  -- 1-bit input: Clear AES Decrypter Key input from Battery-Backed RAM (BBRAM)
-         PACK      => '0',  -- 1-bit input: PROGRAM acknowledge input
-         USRCCLKO  => flashClk,         -- 1-bit input: User CCLK input
-         USRCCLKTS => '0',  -- 1-bit input: User CCLK 3-state enable input
-         USRDONEO  => '1',  -- 1-bit input: User DONE pin output control
-         USRDONETS => '1');  -- 1-bit input: User DONE 3-state enable output              
-
-   GEN_IOBUF :
-   for i in 15 downto 4 generate
-      IOBUF_inst : IOBUF
-         port map (
-            O  => flashDout(i),         -- Buffer output
-            IO => flashData(i),  -- Buffer inout port (connect directly to top-level port)
-            I  => flashDin(i),          -- Buffer input
-            T  => flashTri);  -- 3-state enable input, high=input, low=output     
-   end generate GEN_IOBUF;
+         cardResetIn        => systemReset);
 
    ---------------
    -- AXI PCIe DMA

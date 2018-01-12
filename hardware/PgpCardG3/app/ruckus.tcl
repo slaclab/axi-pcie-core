9f0d1dabfdfa148eb9b4aa510ade7f8d37bb7f78
Larry Ruckman <ruckman@slac.stanford.edu>
2018-01-12 11:03:34 -0800

    removing obsolete source code

--- hardware/PgpCardG3/app/ruckus.tcl
+++
@@ -1,8 +0,0 @@
-# Load RUCKUS environment and library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
-
-# Check if lcls-timing-core submodule exists
-if { [file exists $::env(MODULES)/lcls-timing-core] == 1 } {   
-   loadSource      -dir  "$::DIR_PATH/evr"
-   loadConstraints -dir  "$::DIR_PATH/evr"
-}

989aad42e8fa4e975884cb116b70b89101e3ab83
Larry Ruckman <ruckman@slac.stanford.edu>
2018-03-15 09:23:02 -0700

    updating submodule locks

--- ruckus.tcl
+++ ruckus.tcl
@@ -2,8 +2,8 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for submodule tagging
-if { [SubmoduleCheck {ruckus} {1.5.12} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}   {1.7.1}  ] < 0 } {exit -1}
+if { [SubmoduleCheck {ruckus} {1.6.0} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}   {1.7.1} ] < 0 } {exit -1}
 
 # Check for version 2017.4 of Vivado (or later)
 if { [VersionCheck 2017.4] < 0 } {exit -1}

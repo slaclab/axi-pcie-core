7d10258a1cb1854ed3fb67a5e891a02cb6c4094c
Larry Ruckman <ruckman@slac.stanford.edu>
2018-09-26 21:03:06 -0700

    renaming core to shared

---
+++ shared/ruckus.tcl
@@ -0,0 +1,15 @@
+# Load RUCKUS library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Check for submodule tagging
+if { [SubmoduleCheck {ruckus} {1.6.11} ] < 0 } {exit -1}
+if { [SubmoduleCheck {surf}   {1.8.9}  ] < 0 } {exit -1}
+
+# Check for version 2017.4 of Vivado (or later)
+if { [VersionCheck 2017.4] < 0 } {exit -1}
+
+# Load Source Code
+loadSource -dir  "$::DIR_PATH/rtl"
+
+# Skip the utilization check during placement
+set_param place.skipUtilizationCheck 1

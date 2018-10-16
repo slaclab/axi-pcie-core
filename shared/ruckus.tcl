8ab2b611518d3444eca1ac4953dc6a1a32246368
Larry Ruckman <ruckman@slac.stanford.edu>
2018-10-16 09:32:19 -0700

    adding AxiPcieDmaTb.vhd

--- shared/ruckus.tcl
+++ shared/ruckus.tcl
@@ -2,11 +2,24 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for submodule tagging
-if { [SubmoduleCheck {ruckus} {1.6.11} ] < 0 } {exit -1}
-if { [SubmoduleCheck {surf}   {1.8.9}  ] < 0 } {exit -1}
+if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
+   if { [SubmoduleCheck {ruckus} {1.6.11} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {1.9.0}  ] < 0 } {exit -1}
+} else {
+   puts "\n\n*********************************************************"
+   puts "OVERRIDE_SUBMODULE_LOCKS != 0"
+   puts "Ignoring the submodule locks in axi-pcie-core/ruckus.tcl"
+   puts "*********************************************************\n\n"
+}   
 
 # Load Source Code
-loadSource -dir  "$::DIR_PATH/rtl"
+loadSource            -dir "$::DIR_PATH/rtl"
+loadSource -sim_only  -dir "$::DIR_PATH/tb"
+loadIpCore            -dir "$::DIR_PATH/ip"
+
+# AxiBram only used in simulation
+set_property used_in_implementation false [get_files {AxiBram.xci}]
+set_property used_in_synthesis      false [get_files {AxiBram.xci}]
 
 # Skip the utilization check during placement
 set_param place.skipUtilizationCheck 1

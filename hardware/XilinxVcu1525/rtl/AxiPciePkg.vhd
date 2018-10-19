7fa99a2c7cd2dc279a1f5ffa6f4decd07bfcee61
Larry Ruckman <ruckman@slac.stanford.edu>
2018-10-19 11:58:03 -0700

    updating the XilinxVcu1525 from 48-bit to 40-bit address

--- hardware/XilinxVcu1525/rtl/AxiPciePkg.vhd
+++ hardware/XilinxVcu1525/rtl/AxiPciePkg.vhd
@@ -41,7 +41,7 @@ package AxiPciePkg is
 
    -- PCIE PHY AXI Configuration   
    constant AXI_PCIE_CONFIG_C : AxiConfigType := (
-      ADDR_WIDTH_C => 48,               -- 48-bit address interface
+      ADDR_WIDTH_C => 40,               -- 40-bit address interface
       DATA_BYTES_C => 64,               -- 512-bit data interface
       ID_BITS_C    => 4,                -- Up to 16 DMA IDS
       LEN_BITS_C   => 8);               -- 8-bit awlen/arlen interface      

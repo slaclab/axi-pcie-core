8c3916a9054dbd874cd266845cd02afa36fb152a
Larry Ruckman <ruckman@slac.stanford.edu>
2019-08-14 17:07:43 -0700

    updating KCU1500 appReg to be optional
    useful for simple DMA loopback examples

--- hardware/XilinxKcu1500/rtl/XilinxKcu1500Core.vhd
+++ hardware/XilinxKcu1500/rtl/XilinxKcu1500Core.vhd
@@ -51,17 +51,17 @@ entity XilinxKcu1500Core is
       dmaIbMasters   : in  AxiStreamMasterArray(DMA_SIZE_G-1 downto 0);
       dmaIbSlaves    : out AxiStreamSlaveArray(DMA_SIZE_G-1 downto 0);
       -- PIP Interface [0x00080000:0009FFFF] (dmaClk domain)
-      pipIbMaster    : out AxiWriteMasterType := AXI_WRITE_MASTER_INIT_C;
-      pipIbSlave     : in  AxiWriteSlaveType  := AXI_WRITE_SLAVE_FORCE_C;
-      pipObMaster    : in  AxiWriteMasterType := AXI_WRITE_MASTER_INIT_C;
-      pipObSlave     : out AxiWriteSlaveType  := AXI_WRITE_SLAVE_FORCE_C;
+      pipIbMaster    : out AxiWriteMasterType    := AXI_WRITE_MASTER_INIT_C;
+      pipIbSlave     : in  AxiWriteSlaveType     := AXI_WRITE_SLAVE_FORCE_C;
+      pipObMaster    : in  AxiWriteMasterType    := AXI_WRITE_MASTER_INIT_C;
+      pipObSlave     : out AxiWriteSlaveType     := AXI_WRITE_SLAVE_FORCE_C;
       -- Application AXI-Lite Interfaces [0x00100000:0x00FFFFFF] (appClk domain)
-      appClk         : in  sl;
-      appRst         : in  sl;
+      appClk         : in  sl                    := '0';
+      appRst         : in  sl                    := '1';
       appReadMaster  : out AxiLiteReadMasterType;
-      appReadSlave   : in  AxiLiteReadSlaveType;
+      appReadSlave   : in  AxiLiteReadSlaveType  := AXI_LITE_READ_SLAVE_EMPTY_OK_C;
       appWriteMaster : out AxiLiteWriteMasterType;
-      appWriteSlave  : in  AxiLiteWriteSlaveType;
+      appWriteSlave  : in  AxiLiteWriteSlaveType := AXI_LITE_WRITE_SLAVE_EMPTY_OK_C;
       -------------------
       --  Top Level Ports
       -------------------      

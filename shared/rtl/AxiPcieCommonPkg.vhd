cd861650288e85b04c208e3273f6816bb3067141
Larry Ruckman <ruckman@slac.stanford.edu>
2019-08-21 21:12:16 -0700

    adding package

---
+++ shared/rtl/AxiPcieCommonPkg.vhd
@@ -0,0 +1,42 @@
+-------------------------------------------------------------------------------
+-- File       : AxiPcieCommonPkg.vhd
+-- Company    : SLAC National Accelerator Laboratory
+-------------------------------------------------------------------------------
+-- Description: Package file for AXI PCIe Core
+-------------------------------------------------------------------------------
+-- This file is part of 'axi-pcie-core'.
+-- It is subject to the license terms in the LICENSE.txt file found in the 
+-- top-level directory of this distribution and at: 
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+-- No part of 'axi-pcie-core', including this file, 
+-- may be copied, modified, propagated, or distributed except according to 
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.StdRtlPkg.all;
+use work.AxiLitePkg.all;
+
+package AxiPcieCommonPkg is
+
+   constant APP_BAR0_XBAR_CONFIG_C : AxiLiteCrossbarMasterConfigArray(3 downto 0) := (
+      0               => (
+         baseAddr     => x"0010_0000",
+         addrBits     => 20,
+         connectivity => x"FFFF"),
+      1               => (
+         baseAddr     => x"0020_0000",
+         addrBits     => 21,
+         connectivity => x"FFFF"),
+      2               => (
+         baseAddr     => x"0040_0000",
+         addrBits     => 22,
+         connectivity => x"FFFF"),
+      3               => (
+         baseAddr     => x"0080_0000",
+         addrBits     => 23,
+         connectivity => x"FFFF"));
+
+end package AxiPcieCommonPkg;

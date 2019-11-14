53172e390a2d9b6eca96eac8343c3037d82cfb42
Benjamin Reese <bareese@slac.stanford.edu>
2019-11-14 15:08:49 -0800

    Rewrite class into two separate functions

--- python/axipcie/_AxiPcieDma.py
+++ python/axipcie/_AxiPcieDma.py
@@ -1,17 +1,24 @@
 import rogue
 
-class AxiPcieDma(object):
-    def __init__(self, driverPath, numLanes, destList):
 
-        # Create PCIE memory mapped interface
-        if (driverPath != 'sim'):
-            # BAR0 access
-            self.memMap = rogue.hardware.axi.AxiMemMap(driverPath)
-            self.dmaStreams = [{dest:rogue.hardware.axi.AxiStreamDma(driverPath, (0x100*lane)|dest) for dest in destList} for lane in range(numLanes)]             
-        else:
-            # FW/SW co-simulation
-            self.memMap = rogue.interfaces.memory.TcpClient('localhost',8000)
-            self.dmaStreams = [{dest:rogue.interfaces.stream.TcpClient('localhost', 8002+(512*lane)+2*dest) for dest in destList} for lane in range(numLanes)]
+def createAxiPcieMemMap(driverPath, host='localhost', port=8000):
+    """Provides BAR0 access to AxiPcieCore"""
+    if driverPath != 'sim':
+        return rogue.hardware.axi.AxiMemMap(driverPath)
+    else:
+        return rogue.interfaces.memory.TcpClient(host, port)
+
+def createAxiPcieDmaStreams(driverPath, streamMap, host='localhost', basePort=8000):
+    """Provides DMA stream access for AxiPcieCore"""
+        d = {}
+        for lane, dests in streamMap.items():
+            for dest in dests:
+                if driverPath != 'sim':                
+                    d[lane][dest] = rogue.hardware.axi.AxiStreamDma(driverPath, (0x100*lane)|dest)
+                else:
+                    d[lane][dest] = rogue.interfaces.stream.TcpClient(host, (basePort+2)+(512*lane)+2*dest)
+        return d
+
 
             
 

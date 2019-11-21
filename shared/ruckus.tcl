89f565af8fc4c339ed14a1312693f4b449a65039
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-21 08:21:30 -0800

    updating submodule locks

--- shared/ruckus.tcl
+++ shared/ruckus.tcl
@@ -3,8 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
-   if { [SubmoduleCheck {ruckus} {1.8.1}  ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}   {1.11.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {ruckus} {2.0.1}  ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.0.0} ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"

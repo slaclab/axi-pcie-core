4c45a5e0cfdc5beedfd0801ef930ce95eddaa03a
Benjamin Reese <bareese@slac.stanford.edu>
2019-11-25 14:33:44 -0800

    Software bug fixes

--- python/axipcie/_AxiPcieDma.py
+++ python/axipcie/_AxiPcieDma.py
@@ -1,5 +1,5 @@
 import rogue
-
+from collections import defaultdict
 
 def createAxiPcieMemMap(driverPath, host='localhost', port=8000):
     """Provides BAR0 access to AxiPcieCore"""
@@ -10,14 +10,14 @@ def createAxiPcieMemMap(driverPath, host='localhost', port=8000):
 
 def createAxiPcieDmaStreams(driverPath, streamMap, host='localhost', basePort=8000):
     """Provides DMA stream access for AxiPcieCore"""
-        d = {}
-        for lane, dests in streamMap.items():
-            for dest in dests:
-                if driverPath != 'sim':                
-                    d[lane][dest] = rogue.hardware.axi.AxiStreamDma(driverPath, (0x100*lane)|dest)
-                else:
-                    d[lane][dest] = rogue.interfaces.stream.TcpClient(host, (basePort+2)+(512*lane)+2*dest)
-        return d
+    d = defaultdict(dict)
+    for lane, dests in streamMap.items():
+        for dest in dests:
+            if driverPath != 'sim':                
+                d[lane][dest] = rogue.hardware.axi.AxiStreamDma(driverPath, (0x100*lane)|dest)
+            else:
+                d[lane][dest] = rogue.interfaces.stream.TcpClient(host, (basePort+2)+(512*lane)+2*dest)
+    return d
 
 
             
--- python/axipcie/__init__.py
+++ python/axipcie/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
-from axipcie._AxiPcieCore import *
-from axipcie._AxiPipCore  import *
+from axipcie._AxiPcieCore    import *
+from axipcie._AxiPcieDma     import *
+from axipcie._AxiPipCore     import *
 from axipcie._PcieAxiVersion import *

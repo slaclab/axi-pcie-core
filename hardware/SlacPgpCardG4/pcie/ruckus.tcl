7859c924727a9c95266fa2786ec943ff0ce07041
Larry Ruckman <ruckman@slac.stanford.edu>
2019-12-09 13:53:11 -0800

    adding hardware/SlacPgpCardG4

---
+++ hardware/SlacPgpCardG4/pcie/ruckus.tcl
@@ -0,0 +1,16 @@
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Placeholder for multiple FPGA type support in the future
+set FpgaType "XCKU040"
+
+# Load local Source Code and Constraints
+loadSource -lib axi_pcie_core -dir  "$::DIR_PATH/rtl"
+
+# loadIpCore -path "$::DIR_PATH/ip/${FpgaType}/SlacPgpCardG4PciePhy.xci"
+loadSource -lib axi_pcie_core   -path "$::DIR_PATH/ip/${FpgaType}/SlacPgpCardG4PciePhy.dcp"
+
+loadConstraints -path "$::DIR_PATH/ip/${FpgaType}/SlacPgpCardG4PciePhy.xdc"
+set_property PROCESSING_ORDER {EARLY}                         [get_files {SlacPgpCardG4PciePhy.xdc}]
+set_property SCOPED_TO_REF    {SlacPgpCardG4PciePhy_pcie3_ip} [get_files {SlacPgpCardG4PciePhy.xdc}]
+set_property SCOPED_TO_CELLS  {U0}                            [get_files {SlacPgpCardG4PciePhy.xdc}]

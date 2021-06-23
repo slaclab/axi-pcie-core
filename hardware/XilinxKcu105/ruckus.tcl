bce9796159ad021ff5504f653f2f2a550752303b
Larry Ruckman <ruckman@slac.stanford.edu>
2021-06-23 12:11:36 -0700

    hardware/XilinxKcu105/ruckus.tcl
    `set_property board_part xilinx.com:kcu105:part0:1.6 [current_project]`

--- hardware/XilinxKcu105/ruckus.tcl
+++ hardware/XilinxKcu105/ruckus.tcl
@@ -13,7 +13,7 @@ if { $::env(PRJ_PART) != "XCKU040-FFVA1156-2-E" } {
 }
 
 # Set the board part
-set_property board_part xilinx.com:kcu105:part0:1.3 [current_project]
+set_property board_part xilinx.com:kcu105:part0:1.6 [current_project]
 
 # Load local Source Code and Constraints
 loadSource -lib axi_pcie_core      -dir  "$::DIR_PATH/rtl"

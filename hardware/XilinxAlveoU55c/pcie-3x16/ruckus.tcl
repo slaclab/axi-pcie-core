1de34fc9dd539bd85fd4544a9adfa70a7ee79486
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-22 14:44:18 -0800

    updating XilinxAlveoU55c xdc

--- hardware/XilinxAlveoU55c/pcie-3x16/ruckus.tcl
+++ hardware/XilinxAlveoU55c/pcie-3x16/ruckus.tcl
@@ -9,15 +9,20 @@ loadConstraints               -dir "$::DIR_PATH/xdc"
 
 loadSource -lib axi_pcie_core -path "$::DIR_PATH/ip/XilinxAlveoU55cPciePhyGen3x16.dcp"
 
+loadConstraints -path "$::DIR_PATH/ip/XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc"
+set_property PROCESSING_ORDER {EARLY}                                      [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
+set_property SCOPED_TO_REF    {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt} [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
+set_property SCOPED_TO_CELLS  {inst}                                       [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
+
+loadConstraints -path "$::DIR_PATH/ip/ip_pcie4c_uscale_plus_impl_x1y1.xdc"
+set_property PROCESSING_ORDER {EARLY}                                   [get_files {ip_pcie4c_uscale_plus_impl_x1y1.xdc}]
+set_property SCOPED_TO_REF    {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip} [get_files {ip_pcie4c_uscale_plus_impl_x1y1.xdc}]
+set_property SCOPED_TO_CELLS  {inst}                                    [get_files {ip_pcie4c_uscale_plus_impl_x1y1.xdc}]
+
 loadConstraints -path "$::DIR_PATH/ip/ip_pcie4c_uscale_plus_x1y1.xdc"
 set_property PROCESSING_ORDER {EARLY}                                   [get_files {ip_pcie4c_uscale_plus_x1y1.xdc}]
 set_property SCOPED_TO_REF    {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip} [get_files {ip_pcie4c_uscale_plus_x1y1.xdc}]
 set_property SCOPED_TO_CELLS  {inst}                                    [get_files {ip_pcie4c_uscale_plus_x1y1.xdc}]
-
-loadConstraints -path "$::DIR_PATH/ip/XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc"
-set_property PROCESSING_ORDER {EARLY}                                     [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
-set_property SCOPED_TO_REF    {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt} [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
-set_property SCOPED_TO_CELLS  {inst}                                      [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_gt.xdc}]
 
 loadConstraints -path "$::DIR_PATH/ip/XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_late.xdc"
 set_property PROCESSING_ORDER {LATE}                                   [get_files {XilinxAlveoU55cPciePhyGen3x16_pcie4c_ip_late.xdc}]

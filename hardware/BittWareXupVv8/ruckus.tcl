046677fed4465dba723d1f965e25660ac8333205
Larry Ruckman <ruckman@slac.stanford.edu>
2024-02-09 12:20:01 -0800

    ruckus.tcl clean up

--- hardware/BittWareXupVv8/ruckus.tcl
+++ hardware/BittWareXupVv8/ruckus.tcl
@@ -10,24 +10,20 @@ loadRuckusTcl "$::DIR_PATH/../../shared"
 # Set the target language for Verilog (removes warning messages in PCIe IP core)
 set_property target_language Verilog [current_project]
 
-# Check for valid FPGA part number
-if { $::env(PRJ_PART) == "xcvu13p-figd2104-2-e" } {
-   loadSource -lib axi_pcie_core -dir "$::DIR_PATH/core/VU13P"
-} elseif { $::env(PRJ_PART) == "xcvu9p-fsgd2104-2-e" } {
-   loadSource -lib axi_pcie_core -dir "$::DIR_PATH/core/VU9P"
-} else {
-   puts "\n\nERROR: PRJ_PART was not defined as xcvu13p-figd2104-2-e or xcvu9p-fsgd2104-2-e in the Makefile\n\n"; exit -1
-}
-
 # Load local Source Code and Constraints
 loadSource -lib axi_pcie_core -dir "$::DIR_PATH/core"
 loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8Core.xdc"
 loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8App.xdc"
 
+# Check for valid FPGA part number
 if { $::env(PRJ_PART) == "xcvu13p-figd2104-2-e" } {
-  loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8Vu13p.xdc"
+   loadSource -lib axi_pcie_core -dir "$::DIR_PATH/core/VU13P"
+   loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8Vu13p.xdc"
 } elseif { $::env(PRJ_PART) == "xcvu9p-fsgd2104-2-e" } {
-  loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8Vu9p.xdc"    
+   loadSource -lib axi_pcie_core -dir "$::DIR_PATH/core/VU9P"
+   loadConstraints -path "$::DIR_PATH/xdc/BittWareXupVv8Vu9p.xdc"
+} else {
+   puts "\n\nERROR: PRJ_PART was not defined as xcvu13p-figd2104-2-e or xcvu9p-fsgd2104-2-e in the Makefile\n\n"; exit -1
 }
 
 # Load the PCIe core

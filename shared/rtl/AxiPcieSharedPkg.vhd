c48d56917c90f64f96deefef0441e9c3593a890e
Larry Ruckman <ruckman@slac.stanford.edu>
2024-02-08 16:07:27 -0800

    adding support for hardware/AbacoPc821

--- shared/rtl/AxiPcieSharedPkg.vhd
+++ shared/rtl/AxiPcieSharedPkg.vhd
@@ -24,22 +24,25 @@ use surf.StdRtlPkg.all;
 package AxiPcieSharedPkg is
 
    -- List of PCIe Hardware Types
-   constant HW_TYPE_UNDEFINED_C        : slv(31 downto 0) := x"00_00_00_00";
-   constant HW_TYPE_ALPHADATA_KU3_C    : slv(31 downto 0) := x"00_00_00_01";
-   constant HW_TYPE_BITTWARE_XUP_VV8_C : slv(31 downto 0) := x"00_00_00_02";
-   constant HW_TYPE_SLAC_PGP_GEN3_C    : slv(31 downto 0) := x"00_00_00_03";
-   constant HW_TYPE_SLAC_PGP_GEN4_C    : slv(31 downto 0) := x"00_00_00_04";
-   constant HW_TYPE_XILINX_AC701_C     : slv(31 downto 0) := x"00_00_00_05";
-   constant HW_TYPE_XILINX_U50_C       : slv(31 downto 0) := x"00_00_00_06";
-   constant HW_TYPE_XILINX_U200_C      : slv(31 downto 0) := x"00_00_00_07";
-   constant HW_TYPE_XILINX_U250_C      : slv(31 downto 0) := x"00_00_00_08";
-   constant HW_TYPE_XILINX_U280_C      : slv(31 downto 0) := x"00_00_00_09";
-   constant HW_TYPE_XILINX_KC705_C     : slv(31 downto 0) := x"00_00_00_0A";
-   constant HW_TYPE_XILINX_KCU105_C    : slv(31 downto 0) := x"00_00_00_0B";
-   constant HW_TYPE_XILINX_KCU116_C    : slv(31 downto 0) := x"00_00_00_0C";
-   constant HW_TYPE_XILINX_KCU1500_C   : slv(31 downto 0) := x"00_00_00_0D";
-   constant HW_TYPE_XILINX_VCU128_C    : slv(31 downto 0) := x"00_00_00_0E";
-   constant HW_TYPE_XILINX_U55C_C      : slv(31 downto 0) := x"00_00_00_0F";
-   constant HW_TYPE_XILINX_C1100_C     : slv(31 downto 0) := x"00_00_00_10";
+   constant HW_TYPE_UNDEFINED_C              : slv(31 downto 0) := x"00_00_00_00";
+   constant HW_TYPE_ALPHADATA_KU3_C          : slv(31 downto 0) := x"00_00_00_01";
+   constant HW_TYPE_BITTWARE_XUP_VV8_VU13P_C : slv(31 downto 0) := x"00_00_00_02";
+   constant HW_TYPE_SLAC_PGP_GEN3_C          : slv(31 downto 0) := x"00_00_00_03";
+   constant HW_TYPE_SLAC_PGP_GEN4_C          : slv(31 downto 0) := x"00_00_00_04";
+   constant HW_TYPE_XILINX_AC701_C           : slv(31 downto 0) := x"00_00_00_05";
+   constant HW_TYPE_XILINX_U50_C             : slv(31 downto 0) := x"00_00_00_06";
+   constant HW_TYPE_XILINX_U200_C            : slv(31 downto 0) := x"00_00_00_07";
+   constant HW_TYPE_XILINX_U250_C            : slv(31 downto 0) := x"00_00_00_08";
+   constant HW_TYPE_XILINX_U280_C            : slv(31 downto 0) := x"00_00_00_09";
+   constant HW_TYPE_XILINX_KC705_C           : slv(31 downto 0) := x"00_00_00_0A";
+   constant HW_TYPE_XILINX_KCU105_C          : slv(31 downto 0) := x"00_00_00_0B";
+   constant HW_TYPE_XILINX_KCU116_C          : slv(31 downto 0) := x"00_00_00_0C";
+   constant HW_TYPE_XILINX_KCU1500_C         : slv(31 downto 0) := x"00_00_00_0D";
+   constant HW_TYPE_XILINX_VCU128_C          : slv(31 downto 0) := x"00_00_00_0E";
+   constant HW_TYPE_XILINX_U55C_C            : slv(31 downto 0) := x"00_00_00_0F";
+   constant HW_TYPE_XILINX_C1100_C           : slv(31 downto 0) := x"00_00_00_10";
+   constant HW_TYPE_ABACO_PC821_KU085_C      : slv(31 downto 0) := x"00_00_00_11";
+   constant HW_TYPE_ABACO_PC821_KU115_C      : slv(31 downto 0) := x"00_00_00_12";
+   constant HW_TYPE_BITTWARE_XUP_VV8_VU9P_C  : slv(31 downto 0) := x"00_00_00_13";
 
 end package AxiPcieSharedPkg;

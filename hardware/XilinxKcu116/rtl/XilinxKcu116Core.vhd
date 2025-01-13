826cc8a67fc8921ed00b4f33089eec66b8640f79
mmishra9 <mmishra9@slac.stanford.edu>
2025-01-13 13:59:51 -0800

    DATAGPU_EN_G to hardware

--- hardware/XilinxKcu116/rtl/XilinxKcu116Core.vhd
+++ hardware/XilinxKcu116/rtl/XilinxKcu116Core.vhd
@@ -41,6 +41,7 @@ entity XilinxKcu116Core is
       DMA_AXIS_CONFIG_G    : AxiStreamConfigType;
       DRIVER_TYPE_ID_G     : slv(31 downto 0)            := x"00000000";
       DMA_BURST_BYTES_G    : positive range 256 to 4096  := 256;
+      DATAGPU_EN_G         : boolean                     := false;
       DMA_SIZE_G           : positive range 1 to 8       := 1);
    port (
       ------------------------
@@ -200,6 +201,7 @@ begin
          DRIVER_TYPE_ID_G     => DRIVER_TYPE_ID_G,
          PCIE_HW_TYPE_G       => HW_TYPE_XILINX_KCU116_C,
          DMA_AXIS_CONFIG_G    => DMA_AXIS_CONFIG_G,
+         DATAGPU_EN_G         => DATAGPU_EN_G,
          DMA_SIZE_G           => DMA_SIZE_G)
       port map (
          -- AXI4 Interfaces

c88b4321e7760744719f51831b2470ee0f1701fd
Larry Ruckman <ruckman@slac.stanford.edu>
2026-03-27 15:42:43 -0700

    relating the clock speed to make timing closure easier

--- hardware/XilinxVariumC1100/hbm/HbmDmaBufferV2.vhd
+++ hardware/XilinxVariumC1100/hbm/HbmDmaBufferV2.vhd
@@ -294,7 +294,7 @@ begin
          -- MMCM attributes
          CLKIN_PERIOD_G     => 10.0,    -- 100MHz
          CLKFBOUT_MULT_F_G  => 13.5,    -- 1.35GHz = 13.5 x 100MHz
-         CLKOUT0_DIVIDE_F_G => 3.75)    -- 360MHz = 1.35GHz/3.75
+         CLKOUT0_DIVIDE_F_G => 4.5)     -- 300MHz = 1.35GHz/4.5
       port map(
          -- Clock Input
          clkIn     => userClk,

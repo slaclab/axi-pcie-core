f59b58498ff0c15db3b2d43eb575107a8c1c97ed
Larry Ruckman <ruckman@slac.stanford.edu>
2026-05-14 13:16:26 -0700

    updating surf submodule lock to v2.71.0

--- shared/ruckus.tcl
+++ shared/ruckus.tcl
@@ -4,7 +4,7 @@ source $::env(RUCKUS_PROC_TCL)
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {ruckus} {4.24.2} ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}   {2.69.0} ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.71.0} ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"

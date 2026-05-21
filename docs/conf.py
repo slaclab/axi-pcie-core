38f20235bc4cb5c1888147364ddc7a9251ae8e7d
Larry Ruckman <ruckman@slac.stanford.edu>
2026-05-20 22:23:32 -0700

    docs: switch Sphinx theme to sphinx_rtd_theme to match slaclab style

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,24 @@
 # Configuration file for the Sphinx documentation builder.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import subprocess
 
 project = 'axi-pcie-core'
 author = 'SLAC National Accelerator Laboratory'
 copyright = '2026, SLAC National Accelerator Laboratory'
-release = ''
 
-extensions = ['sphinx.ext.extlinks']
+try:
+    release = subprocess.check_output(
+        ['git', 'describe', '--tags', '--abbrev=0'],
+        stderr=subprocess.DEVNULL,
+    ).decode().strip()
+except Exception:
+    release = 'dev'
+version = release
+
+extensions = [
+    'sphinx.ext.extlinks',
+    'sphinx_copybutton',
+]
 
 extlinks = {
     'repo': ('https://github.com/slaclab/axi-pcie-core/blob/main/%s', '%s'),
@@ -19,5 +31,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 language = 'en'
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'titles_only': True, 'navigation_depth': -1}
+html_title = 'axi-pcie-core'
+html_baseurl = 'https://slaclab.github.io/axi-pcie-core/'
 html_static_path = ['_static']
